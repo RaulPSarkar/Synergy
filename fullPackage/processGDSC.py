007381e4b36da89e9049c5c60c94ee1e5771f98e
RaulPSarkar <raul.sarkar@tecnico.pt>
2023-06-11 17:47:01 +0100

    crispr

--- fullPackage/processGDSC.py
+++ fullPackage/processGDSC.py
@@ -15,7 +15,8 @@ pancreasPath = Path(__file__).parent / "datasets/pancreas_anchor_combo.csv.gz"
 colonPath = Path(__file__).parent / "datasets/colon_anchor_combo.csv.gz"
 breastPath = Path(__file__).parent / "datasets/breast_anchor_combo.csv.gz"
 drugCached = Path(__file__).parent / "datasets/drug2smiles.txt"
-
+omicsData = Path(__file__).parent / "datasets/crispr.csv"
+outputFile = Path(__file__).parent / "datasets/processedCRISPR.csv"
 ##########################
 ##########################
 
@@ -26,7 +27,7 @@ pancreas = pd.read_csv(pancreasPath)
 colon = pd.read_csv(colonPath)
 breast = pd.read_csv(breastPath)
 full = pd.concat([pancreas,colon, breast], axis=0)
-#JOIN ALL DATASETS
+#JOINS ALL DATASETS
 
 
 
@@ -61,41 +62,17 @@ out2 = out.merge(smilesTable, left_on='Anchor Name', right_on='drug')
 out2.rename(columns={'Library Name': 'NSC1', 'Anchor Name': 'NSC2', 'SDIM': 'CELLNAME'}, inplace=True)
 
 out2['GROUP'] = out2['NSC1'] + '_' + out2['NSC2']
-#out2.to_csv('dataset.csv')
 
 
-
-crispr = pd.read_csv('transcriptomics.csv', index_col=0)
+crispr = pd.read_csv(omicsData, index_col=0)
 crispr = crispr.fillna(0)
 crispr.columns.name = 'CELLNAME'
 crisprT = crispr.T
-
-#crisprT = pd.read_csv('crisprT.csv')
-#crispr.T.to_csv('crisprT.csv')
-
-crispr.T.to_csv('transcriptomicsT.csv')
-
-
-print("here")
-
+#crispr.T.to_csv('transcriptomicsT.csv')
 
 df = crisprT.merge(out2, on=['CELLNAME'])
 
 df = df[['Tissue', 'CELLNAME', 'NSC1', 'NSC2', 'Anchor Conc', 'SMILES_A', 'SMILES_B', 'GROUP', 'Delta Xmid', 'Delta Emax']]
-
-#df.rename(columns={'Synergy?': 'COMBOSCORE'}, inplace=True)
-
-
-#dgi = pd.read_csv('dgi.csv')
-#columnNames = []
-
-#for column in dgi.columns:
-#    if column in crisprT.columns:
-#        columnNames.append(column)
-#crisprT = crisprT[columnNames]
-
-#print(crisprT)
-#crisprT.to_csv('crisprT.csv')
 
 df.drop_duplicates(inplace=True)
 
