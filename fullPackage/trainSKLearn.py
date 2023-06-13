a63416c785d166b00f6899b0bef234837f560c38
RaulPSarkar <raul.sarkar@tecnico.pt>
2023-06-13 17:07:42 +0100

    almost perfect

--- fullPackage/trainSKLearn.py
+++ fullPackage/trainSKLearn.py
@@ -18,6 +18,7 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.linear_model import ElasticNet
 from sklearn.svm import LinearSVR
 from lightgbm import LGBMRegressor
+from xgboost import XGBRegressor
 
 
 
@@ -26,14 +27,14 @@ from lightgbm import LGBMRegressor
 ##########################
 ##########################
 #Change
-modelName = 'en' #en, rf, lgbm, svr
+modelName = 'lgbm' #en, rf, lgbm, svr, xgboost
 data = Path(__file__).parent / 'datasets/processedCRISPR.csv'
 omics = Path(__file__).parent / 'datasets/crispr.csv.gz'
 fingerprints = Path(__file__).parent / 'datasets/smiles2fingerprints.csv'
 landmarkList = Path(__file__).parent / 'datasets/landmarkgenes.txt'
-outputPredictions = Path(__file__).parent / 'predictions/EN'
+outputPredictions = Path(__file__).parent / 'predictions'
 tunerDirectory = Path(__file__).parent / 'tuner'
-tunerTrials = 2
+tunerTrials = 1
 kFold = 5
 ##########################
 ##########################
@@ -86,10 +87,16 @@ def build_model(hp):
             subsample_freq = hp.Choice('subsample_freq', ['0','1', '5'])
         ) )  
 
-
-    #'XGBRegressor': XGBRegressor(),
-    #'LGBMRegressor': 
-
+    elif(use=='xgboost'):
+        model = MultiOutputRegressor (XGBRegressor(
+            n_jobs=-1,
+            n_estimators=hp.Int('n_estimators', 100, 1000),
+            learning_rate = hp.Float('learning_rate', 1e-4, 1e-1, sampling="log"),
+            max_depth = hp.Int('max_depth', 3, 9),
+            min_child_weight = hp.Int('min_child_weight', 1, 5),
+            gamma = hp.Float('gamma', 0, 2),
+            subsample = hp.Float('subsample', 0.6, 1.0)
+        ))
 
     return model
 
@@ -101,7 +108,6 @@ def datasetToInput(data, omics, drugs):
     interceptionGenes = []
     for gene in landmarkList['pr_gene_symbol']:
         if gene in omics.T.columns:
-            print(gene)
             interceptionGenes.append(gene)
 
     omicsFinal = omics.T[  interceptionGenes  ]
@@ -114,7 +120,6 @@ def datasetToInput(data, omics, drugs):
     setWithDrugA = setWithOmics.merge(drugs, on='SMILES_A')
     print("Now merging with drug B...")
     fullSet = setWithDrugA.merge(drugs, left_on='SMILES_B', right_on='SMILES_A')
-    print(fullSet)
 
     return fullSet
 
@@ -127,23 +132,23 @@ fullSet = datasetToInput(data,omics, fingerprints)
 #Taken from https://stackoverflow.com/questions/19071199/drop-columns-whose-name-contains-a-specific-string-from-pandas-dataframe because I'm lazy
 X = fullSet.loc[:,~fullSet.columns.str.startswith('SMILES')]
 X = X.loc[:,~X.columns.str.startswith('drug')]
+X = X.loc[:,~X.columns.str.startswith('Unnamed')]
 X = X.drop(['Tissue','CELLNAME','NSC1','NSC2','Anchor Conc','GROUP','Delta Xmid','Delta Emax','mahalanobis'], axis=1)
 
-y = fullSet[ 'Delta Xmid'] #, 'Delta Emax']
+y = fullSet[ ['Delta Xmid', 'Delta Emax' ] ]
 
 
-
+print(y)
 
 
 #cross validation
-
 kf = KFold(n_splits=kFold)
-#model = LogisticRegression(solver= 'liblinear')
 
+fullPredictions = []
 index = 0
 for train_index , test_index in kf.split(X):
     X_train , X_test = X.iloc[train_index,:],X.iloc[test_index,:]
-    y_train , y_test = y[train_index] , y[test_index]
+    y_train , y_test = y.iloc[train_index, :] , y.iloc[test_index, :] #change if just 1 output var y[train_index]
     
 
 
@@ -156,39 +161,40 @@ for train_index , test_index in kf.split(X):
         scoring=metrics.make_scorer(metrics.mean_squared_error),
         cv=model_selection.KFold(5),
         directory= tunerDirectory,
-        project_name=modelName)
+        project_name=modelName + '/' + str(index))
 
-    tuner.search(X_train, y_train)
+
+    print('X:')
+    print(X_train)
+    print('y:')
+    print(y_train)
+
+    
+    tuner.search(X_train, y_train.to_numpy())
     best_model = tuner.get_best_models(num_models=1)[0]
+
 
     ypred = best_model.predict(X_test)
 
 
     df = pd.DataFrame(data={#'experiment': test_dataset.get_row_ids(sep='+'),
-                            #'Tissue': test_dataset.response_dataset['Tissue'],
-                            #'Conc': test_dataset.response_dataset['Anchor Conc'],
-                            'y_trueIC': y_test[:,0],
-                            'y_trueEmax': y_test[:,1],
+                            'Tissue': fullSet['Tissue'],
+                            'Conc': fullSet['Anchor Conc'],
+                            'y_trueIC': y_test.iloc[:,0],
+                            'y_trueEmax': y_test.iloc[:,1],
                             'y_predIC': ypred[:,0],
                             'y_predEmax': ypred[:,1]})
 
-    df.to_csv(outputPredictions / modelName / index / '.csv', index=False)
+
+    saveTo = modelName + str(index) + '.csv'
+    df.to_csv(outputPredictions / saveTo, index=False)
     index+=1
+    fullPredictions.append(df)
 
 
-
-    #model.fit(X_train,y_train)
-    #pred_values = model.predict(X_test)
-    
-    #acc = accuracy_score(pred_values , y_test)
-    #acc_score.append(acc)
-
-
-
-
-
-#loop hyperparameters/train
-
+totalPreds = pd.concat(fullPredictions, axis=0)
+finalName = modelName + '.csv'
+df.to_csv(outputPredictions / 'final' / finalName, index=False)
 
 
 #leave one out validation
