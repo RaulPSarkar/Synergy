031886e5845d088a50579108d677229b400cd9de
RaulPSarkar <raul.sarkar@tecnico.pt>
2023-06-15 11:08:50 +0100

    transcriptomics file

--- fullPackage/trainSKLearn.py
+++ fullPackage/trainSKLearn.py
@@ -27,14 +27,14 @@ from sklearn.model_selection import train_test_split
 ##########################
 ##########################
 #Change
-modelName = 'xgboost' #en, rf, lgbm, svr, xgboost
+modelName = 'rf' #en, rf, lgbm, svr, xgboost
 data = Path(__file__).parent / 'datasets/processedCRISPR.csv'
 omics = Path(__file__).parent / 'datasets/crispr.csv.gz'
 fingerprints = Path(__file__).parent / 'datasets/smiles2fingerprints.csv'
 landmarkList = Path(__file__).parent / 'datasets/landmarkgenes.txt'
 outputPredictions = Path(__file__).parent / 'predictions'
 tunerDirectory = Path(__file__).parent / 'tuner'
-tunerTrials = 22
+tunerTrials = 20
 tunerRun = 0 #increase if you want to start the hyperparameter optimization process anew
 kFold = 5
 useBaselineInstead = False #change to true to use baseline instead of the model from modelName
