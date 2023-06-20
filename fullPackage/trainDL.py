d8fc68ede1276dbfedcd89dd450f7b0141067acc
RaulPSarkar <raul.sarkar@tecnico.pt>
2023-06-20 17:06:52 +0100

    closer

--- fullPackage/trainDL.py
+++ fullPackage/trainDL.py
@@ -79,7 +79,7 @@ y = fullSet[ ['Delta Xmid', 'Delta Emax' ] ]
 
 
 #940 landmark genes
-model = buildDL(940, 1024, 10, 10, 10)
+model = buildDL(940, 1024, '[10]', '[10]', '[10]')
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
