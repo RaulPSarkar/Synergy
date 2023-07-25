1603f0347612568c3fb2f34f2b9bd9c8a1c9f419
RaulPSarkar <raul.sarkar@tecnico.pt>
2023-07-25 17:56:23 +0100

    oops

--- fullPackage/batch/trainNewest.sh
+++ fullPackage/batch/trainNewest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python3 ../trainSKLearn.py -m lgbm -o proteomics -run 100
-python3 ../trainSKLearn.py -m lgbm -o ge -run 100
-python3 ../trainSKLearn.py -m dlCoeffs -run 101
-python3 ../trainSKLearn.py -m xgboost -trials 12 -run 100
+python3 ../trainModel.py -m lgbm -o proteomics -run 100
+python3 ../trainModel.py -m lgbm -o ge -run 100
+python3 ../trainModel.py -m dlCoeffs -run 101
+python3 ../trainModel.py -m xgboost -trials 12 -run 100
 
 
 Echo
