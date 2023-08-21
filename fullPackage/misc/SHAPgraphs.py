85dbb88469c2edf738ab62faaefc617fcc7e20a2
RaulPSarkar <raul.sarkar@tecnico.pt>
2023-08-22 00:06:12 +0300

    udaptpeds

--- fullPackage/misc/SHAPgraphs.py
+++ fullPackage/misc/SHAPgraphs.py
@@ -11,24 +11,32 @@ import os
 ##########################
 
 predictionsDF = Path(__file__).parent / 'predictions/final/lgbm/lgbmrun115regularplusSingleplusCoeffsplusCType.csv' #very important to determine samples used
-shapValuesDF = Path(__file__).parent / '../predictions/final/lgbm/lgbmrun115SHAPvaluesEmax.parquet.gzip'
-localSelection = ['A', 'B'] #if wanting local (only SHAP for a certain drug pair)
+shapValuesIC50 = Path(__file__).parent / '../predictions/final/lgbm/lgbmrun115SHAPvaluesIC50.parquet.gzip'
+shapValuesEmax = Path(__file__).parent / '../predictions/final/lgbm/lgbmrun115SHAPvaluesEmax.parquet.gzip'
+filterBy = ['Tissue'] #if wanting local (only SHAP for a certain drug pair/tissue)
+joinIC50andEmax = False #whether to join these SHAP values onto a single value (useless for now)
 numberOfTopFeatures = 20 #number of most important features to select
 
 ##########################
 ##########################
 
-shapValuesDF = pd.read_parquet(shapValuesDF)
 
-#ass = Path(__file__).parent / '../predictions/final/lgbm/lgbmrun115SHAPvaluesIC50.parquet.gzip'
-#shapValuesDF.to_parquet(ass, compression='gzip')  
-#shapValuesDF.to_csv(ass)
+def filterRowsByProperty(property, predictionsDF):
+    pass
 
+predictionsDF = pd.read_csv(predictionsDF, index_col=0)
+shapValuesIC50 = pd.read_parquet(shapValuesIC50)
+shapValuesEmax = pd.read_parquet(shapValuesEmax)
 
-print(shapValuesDF.head(5))
+shapValuesIC50.columns = predictionsDF.index
+shapValuesEmax.columns = predictionsDF.index
 
+print(shapValuesIC50)
 
-globalValues = shapValuesDF.abs().mean(axis=1)
+globalValues = shapValuesIC50.abs().mean(axis=1)
 topFeatures = globalValues.sort_values( ascending=False).head(numberOfTopFeatures)
 #top 10 most important featurees, as determined by mean absolute SHAP
 print(topFeatures)
+
+print("Breast")
+
